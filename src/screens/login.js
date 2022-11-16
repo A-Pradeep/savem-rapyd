cead7c5f1a8c4aeda100ed31ed6e4780d8495765
Pradeep A <sharmapradeep872@gmail.com>
2022-11-16 23:02:51 +0530

    refactor code

--- src/screens/login.js
+++ src/screens/login.js
@@ -18,12 +18,12 @@ export default function LoginScreen() {
         </div>
 
         <div
-          className="bg-white w-full md:max-w-md lg:max-w-full md:mx-auto md:mx-0 md:w-1/2 xl:w-1/3 h-screen px-6 lg:px-16 xl:px-12
+          className="bg-white w-full md:max-w-md lg:max-w-full md:mx-auto md:w-1/2 xl:w-1/3 h-screen px-6 lg:px-16 xl:px-12
         flex items-center justify-center"
         >
           <div className="w-full h-100">
             <h1 className="text-xl md:text-2xl font-bold leading-tight mt-12">
-              Log in to your account
+              LogIn
             </h1>
 
             <form className="mt-6" action="#" method="POST">
@@ -31,8 +31,8 @@ export default function LoginScreen() {
                 <label className="block text-gray-700">Email Address</label>
                 <input
                   type="email"
-                  name=""
-                  id=""
+                  name="email"
+                  id="email"
                   placeholder="Enter Email Address"
                   className="w-full px-4 py-3 rounded-lg bg-gray-200 mt-2 border focus:border-blue-500 focus:bg-white focus:outline-none"
                   autofocus
@@ -45,8 +45,8 @@ export default function LoginScreen() {
                 <label className="block text-gray-700">Password</label>
                 <input
                   type="password"
-                  name=""
-                  id=""
+                  name="password"
+                  id="password"
                   placeholder="Enter Password"
                   minlength="6"
                   className="w-full px-4 py-3 rounded-lg bg-gray-200 mt-2 border focus:border-blue-500
@@ -67,10 +67,10 @@ export default function LoginScreen() {
             <hr className="my-6 border-gray-300 w-full" />
 
             <p className="mt-8">
-              Need an account?{" "}
+              Need an account?
               <Link
                 to={"/register"}
-                className="text-blue-500 hover:text-blue-700 font-semibold"
+                className="text-blue-500 hover:text-blue-700 font-semibold ml-2"
               >
                 Create an account
               </Link>
--- src/screens/register.js
+++ src/screens/register.js
@@ -18,7 +18,7 @@ export default function RegisterScreen() {
         </div>
 
         <div
-          className="bg-white w-full md:max-w-md lg:max-w-full md:mx-auto md:mx-0 md:w-1/2 xl:w-1/3 h-screen px-6 lg:px-16 xl:px-12
+          className="bg-white w-full md:max-w-md lg:max-w-full md:mx-auto md:w-1/2 xl:w-1/3 h-screen px-6 lg:px-16 xl:px-12
         flex items-center justify-center"
         >
           <div className="w-full h-100">
@@ -31,8 +31,8 @@ export default function RegisterScreen() {
                 <label className="block text-gray-700">Email Address</label>
                 <input
                   type="email"
-                  name=""
-                  id=""
+                  name="email"
+                  id="email"
                   placeholder="Enter Email Address"
                   className="w-full px-4 py-3 rounded-lg bg-gray-200 mt-2 border focus:border-blue-500 focus:bg-white focus:outline-none"
                   autofocus
@@ -44,10 +44,10 @@ export default function RegisterScreen() {
               <div className="pt-2">
                 <label className="block text-gray-700">Full Name</label>
                 <input
-                  type="email"
-                  name=""
-                  id=""
-                  placeholder="Enter Email Address"
+                  type="fullName"
+                  name="fullName"
+                  id="fullName"
+                  placeholder="Full Name"
                   className="w-full px-4 py-3 rounded-lg bg-gray-200 mt-2 border focus:border-blue-500 focus:bg-white focus:outline-none"
                   autofocus
                   autocomplete
@@ -59,8 +59,8 @@ export default function RegisterScreen() {
                 <label className="block text-gray-700">Password</label>
                 <input
                   type="password"
-                  name=""
-                  id=""
+                  name="password"
+                  id="password"
                   placeholder="Enter Password"
                   minlength="6"
                   className="w-full px-4 py-3 rounded-lg bg-gray-200 mt-2 border focus:border-blue-500
@@ -86,7 +86,7 @@ export default function RegisterScreen() {
                 to={"/login"}
                 className="text-blue-500 hover:text-blue-700 font-semibold ml-2"
               >
-                Create an account
+                LogIn
               </Link>
             </p>
           </div>
